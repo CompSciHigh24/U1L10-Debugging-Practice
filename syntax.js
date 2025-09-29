ebb8cb9fbb5abd30407a9b3c8d858ed78be1d3c3
CompSciHigh24 <csh-github@compscihigh.org>
2025-09-29 13:03:21 -0400

    Create syntax.js

---
+++ syntax.js
@@ -0,0 +1,43 @@
+// Uncomment the following lines one at a time, debug, and test your fixes.
+
+/* ========================
+    This file is called syntax.js
+    run your code with 
+    node syntax.js
+========================= */
+
+// Syntax Errors
+
+// function add(a, b {
+//     return a + b;
+// }
+
+// const result = add(5, 7);
+// console.log(result);
+
+
+
+
+
+
+// if (true {
+//     console.log("HELLO");
+// }
+
+
+
+
+// let x = 10
+// let y = 20;
+// if (x > 5) {
+//     console.log(x is greater than 5);
+// }
+
+
+
+
+
+// const = rock {
+//     name: 'pyrite',
+//     age: 67,
+// }
