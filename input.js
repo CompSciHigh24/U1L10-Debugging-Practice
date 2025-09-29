8055ca4794abf562a09c8d5fbbf236f9226a1747
CompSciHigh24 <csh-github@compscihigh.org>
2025-09-29 13:00:44 -0400

    Create input.js

---
+++ input.js
@@ -0,0 +1,45 @@
+// Uncomment the following lines one at a time, debug, and test your fixes.
+
+/* ========================
+    This file is called syntax.js
+    run your code with 
+    node input.js
+========================= */
+
+// Input Errors
+
+
+// function calculateTotal(price, quantity) {
+//     return price * quantity - discount;
+// }
+
+
+
+
+// let totalCost = calculateTotal(10, 2); 
+// console.log(totalCost)
+
+
+
+
+// let totalCost2 = calculateTotal("ten", 2, 5); 
+// console.log(totalCost2)
+
+
+
+
+// function hello(name, age, city) {
+//     return "Hello, my name is " + name + ", I am " + age + " years old, and I live in city";
+// }
+
+
+
+
+// let greeting1 = hello("Alice"); 
+// console.log(greeting);
+
+
+
+
+// let greeting2 = hello("New York", 30, "Alice"); 
+// console.log(greeting);
