3bc5a9a8105606c88ef8b682d1f1c81a06d024bc
CompSciHigh24 <csh-github@compscihigh.org>
2025-09-29 13:01:38 -0400

    Create reference.js

---
+++ reference.js
@@ -0,0 +1,46 @@
+// Uncomment the following lines one at a time, debug, and test your fixes.
+
+/* ========================
+    This file is called syntax.js
+    run your code with 
+    node reference.js
+========================= */
+
+// Reference Errors
+
+
+
+// console.log(lesson)
+// const lesson = "U1L9";
+
+
+
+
+// const activity = "practice";
+// console.log(actrivity)
+
+
+
+
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+// console.log(freet("Cesar"))
+
+
+
+
+
+// function car() {
+//     let model = "Tesla";
+    
+//     if (model === "Tesla") {
+//         let year = 2021;
+//     } else {
+//         let year = 2024;
+//     }
+
+//     return year
+// }
+
+// car();
